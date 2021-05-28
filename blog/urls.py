ba741152f73cad331c9c14a4355183b9890a683b
kimjihun <66763460+kimjihun-dev@users.noreply.github.com>
2021-05-29 00:48:28 +0900

    태그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view()),
-    path('<int:pk>/', views.PostDetail.as_view()),
+    path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
 ]
